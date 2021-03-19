22b8892bfbc14b5e76fc091d63da3540cce134e4
y4shx <40716410+y4shx@users.noreply.github.com>
2021-03-19 13:49:30 +0530

    Add files via upload

---
+++ E-autoscipt.ps1
@@ -0,0 +1,47 @@
+param(
+ 
+ [string]
+ $subscriptionId = "7d4ba3e7-70a5-4d25-ad37-400d3c3d7c83",
+
+
+ [string]
+ $resourceGroupName = "e-1",
+
+ [string]
+ $resourceGroupLocation = "eastus",
+
+ 
+ [string]
+ $deploymentName = "testing",
+
+ [string]
+ $templateFilePath = "$(System.DefaultWorkingDirectory)/_y4shx_Automation/etrack.json",
+
+ [string]
+ $parametersPath = "$(System.DefaultWorkingDirectory)/_y4shx_Automation/Eteack-para.json"
+)
+
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+# Start the deployment
+Write-Host "Starting deployment...";
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $ParametersPath
+
+
+
